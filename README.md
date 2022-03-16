b68867ab41c926d7fcdae46884466a7f549573f5
santos <ronireiosantos@gmail.com>
2022-03-17 01:08:20 +0300

    - show card/event display

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## Next JS React Web
+
+- We want to make sure we recommend events a user would be interested in, in order to do this, 
+  we need to have some information from a user about his interests. We can capture this in 
+  different ways.
+
+
+- On signing-up, we request them to checkmark some categories/topics like AI, robotics, etc. 
+  This can also be done later in settings/profile.
+
+
+We build a recommendation algorithm to find similarities between these user interests and find 
+events that match these interests (using a recommendation algorithm would also help in 
+discovering other events that might be on interest to the user even when they did not chose it 
+as an interesting topic).
+
+---
+As the frontend will only be showing events that have been recommended or that are of interest 
+to the user, the recommendation logic is implemented in the API https://github.com/reiosantos/fig-finance-api
+
+
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,19 +4,29 @@ import { LoaderState, State } from '../../shared/interface';
 import { connect } from 'react-redux';
 
 function Dashboard() {
+  const cards = new Array(6).fill(0);
+
   return (
-    <React.Fragment>
-      <h1 className="text-green font-medium text-large text-center w-full">
-        Welcome to <a href="/">Your Dashboard!</a>
-      </h1>
-      <h2 className="font-medium text-blue">
-        Start by designing your layout in: <code>src/shared/components/layout/index.tsx</code>
-      </h2>
-      <h3>
-        You can utilize both Material-UI and Styled components,
-        both are configured for you consumption.
-      </h3>
-    </React.Fragment>
+    <div className="grid grid-cols-3 gap-2 p-4">
+      {cards.map((c, idx) => (
+        <div className="rounded overflow-hidden shadow-md" key={idx}>
+          <img className="w-full" src="/images/card.png" alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
+              Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
